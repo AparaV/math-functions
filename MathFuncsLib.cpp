d034feba51cceced618ee1b3773c544730cc9379
Apara <ssapar@gmail.com>
2017-03-14 10:37:30 -0600

    Fibonacci list signal end with -1

--- MathFuncsLib.cpp
+++ MathFuncsLib.cpp
@@ -112,12 +112,13 @@ namespace Sequences {
 	}
 	//Fibonacci List
 	int64_t* fibonacciList(int n) {
-		int64_t* list = new int64_t[n + 1];
+		int64_t* list = new int64_t[n + 2];
 		list[0] = 0;
 		list[1] = 1;
 		for (int i = 2; i <= n; ++i) {
 			list[i] = list[i - 1] + list[i - 2];
 		}
+		list[n + 1] = -1;
 		return list;
 	}
 
